0648b85a2babec93fb70c60630ae4773d07cd964
Daniel Alves <alvesdaniel@ua.pt>
2023-05-29 20:44:42 +0100

    Add files via upload

--- pommofinal.ino
+++ pommofinal.ino
@@ -1,24 +1,53 @@
-// Constantes
+//potenciometro
 const int pinPotenciometro = A0;
-const int intervalosSessao[4] = {15, 20, 25, 30};
+int valorPotencia = 0;
+
+//intervalos
+const int listaIntervalosSessao[4] = {1, 2, 3, 4};
+int indiceIntervaloSessao = 0;
+unsigned long intervaloPausa = 0;
+unsigned long intervaloSessao = 0;
+unsigned long tempoAtual = 0;
+
+//ciclos
 const int numCiclo[4] = {1, 2, 3, 4};
+int cicloAtual = 0;
+
+//botoes
 const int botaoIniciar = 2;
 const int botaoInterromper = 3;
+
+//leds
 const int ledPinVerde = 12;
 const int ledPinVermelho = 13;
-const int pinFases[4] = {7, 8, 9, 10};
-const int pinInterrupt = 11;
-const unsigned long tempoPausa = 5 * 60000; //5 minutos em milisegundos
+const int pinCiclos[4] = {7, 8, 9, 10};
+const int pinInterrompido = 11;
+bool ledVerdeLigado = false;
+bool ledVermelhoLigado = false;
 
-// Variáveis globais
-int ciclo = 0;
-int intervalo = 0;
-int valorPotencia = 0;
-unsigned long startTime = 0;
-bool timerIniciado = false;
-bool primeiroClique = false;
-unsigned long tempoPausaInicio = 0;
+//Definição de estados
+enum State {
+  INICIAL,
+  FOCO,
+  PAUSA,
+  INTERROMPIDO
+};
 
+//Variável de estado
+State estadoAtual = INICIAL;
+State estadoAnterior = INICIAL;
+
+long tempoAnterior = 0;
+
+//flags periodo pausa/foco
+bool sessaoIniciada = false;
+bool pausaIniciada = false;
+
+unsigned long inicioFoco = 0;  // Variável para registar o início do período de foco
+unsigned long inicioPausa = 0;  // Variável para registar o início do período de pausa
+unsigned long tempoPassado = 0;  // Variável para armazenar o tempo decorrido no período de foco
+
+//setup I/O
 void setup() {
   // Configuração dos pins
   pinMode(pinPotenciometro, INPUT);
@@ -27,109 +56,186 @@ void setup() {
   pinMode(ledPinVerde, OUTPUT);
   pinMode(ledPinVermelho, OUTPUT);
   for (int i = 0; i < 4; i++) {
-    pinMode(pinFases[i], OUTPUT);
+    pinMode(pinCiclos[i], OUTPUT);
   }
-  pinMode(pinInterrupt, OUTPUT);
-  
+  pinMode(pinInterrompido, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  valorPotencia = analogRead(pinPotenciometro);
-
-  // Verifica o estado dos botões
-  bool botaoIniciarPressionado = digitalRead(botaoIniciar) == HIGH;
-  bool botaoInterromperPressionado = digitalRead(botaoInterromper) == HIGH;
-
-  if (botaoInterromperPressionado) {
-    if (timerIniciado) {
-      // Armazena o tempo decorrido antes da suspensão
-      unsigned long tempoPassado = millis() - startTime;
-      startTime = millis();
-      timerIniciado = false;
-    }
-    primeiroClique = false;
-    digitalWrite(pinInterrupt, HIGH);
-  }
-
-  // Determina o intervalo com base no valor do potenciômetro
-  if (valorPotencia >= 0 && valorPotencia < 256) {
-    intervalo = intervalosSessao[0];
-  } else if (valorPotencia >= 256 && valorPotencia < 512) {
-    intervalo = intervalosSessao[1];
-  } else if (valorPotencia >= 512 && valorPotencia < 768) {
-    intervalo = intervalosSessao[2];
-  } else if (valorPotencia >= 768 && valorPotencia <= 1023) {
-    intervalo = intervalosSessao[3];
-  }
-
-  unsigned long tempoMilisegundos = intervalo * 60000; // intervalo x quantos ms existem num minuto = tempo em ms
-
-  if (botaoIniciarPressionado) {
-    primeiroClique = true;
-    digitalWrite(pinInterrupt, LOW);
-  }
-
-  if (botaoIniciarPressionado && primeiroClique) {
-    unsigned long tempoPassado = 0;
-    if (!timerIniciado) {
-      // Ajusta o tempo de início subtraindo o tempo decorrido
-      startTime = millis() - tempoPassado;
-    }
-    timerIniciado = true;
-    primeiroClique = false;
-    Serial.println("Timer iniciado!");
-    atualizarLEDs(true, false);
-  }
-
-  if (timerIniciado && millis() - startTime >= tempoMilisegundos) {
-    timerIniciado = false;
-    atualizarLEDs(true, false);
-    ciclo += 1;
-
-    if (ciclo < 4) {
-      primeiroClique = true;
-      tempoPausaInicio = millis();
-    } else if (ciclo == 4) {
-      primeiroClique = true;
-      tempoPausaInicio = millis();
-      desligarFases();
-    } else {
-      ciclo = 0;
-      primeiroClique = true;
-    }
-  }
-
-  if (primeiroClique && millis() - tempoPausaInicio >= tempoPausa) {
-    primeiroClique = false;
-    startTime = millis();
-    timerIniciado = true;
-    Serial.println("Pausa iniciada!");
-    atualizarLEDs(true, false);
-  }
-
-  if (timerIniciado) {
-    unsigned long tempoPassado = millis() - startTime;
-    Serial.print("Tempo decorrido: ");
-    Serial.print(tempoPassado);
-    Serial.println(" ms");
-    atualizarLEDs(false, true);
-  }
-
-  if (timerIniciado && ciclo >= 0 && ciclo < 4) {
-    for (int i = 0; i <= ciclo; i++) {
-      digitalWrite(pinFases[i], HIGH);
-    }
-  }
-}
-
+//atualiza ledbicolor
 void atualizarLEDs(bool ledVerdeLigado, bool ledVermelhoLigado) {
   digitalWrite(ledPinVerde, ledVerdeLigado ? HIGH : LOW);
   digitalWrite(ledPinVermelho, ledVermelhoLigado ? HIGH : LOW);
 }
 
-void desligarFases() {
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pinFases[i], LOW);
+void atualizarLEDsCiclos(int cicloAtual) {
+  switch (cicloAtual) {
+    case 0:
+      for (int i = 0; i < 4; i++) {
+        digitalWrite(pinCiclos[i], LOW);
+      }
+      break;
+    case 1:
+      digitalWrite(pinCiclos[0], HIGH);
+      digitalWrite(pinCiclos[1], LOW);
+      digitalWrite(pinCiclos[2], LOW);
+      digitalWrite(pinCiclos[3], LOW);
+      break;
+    case 2:
+      digitalWrite(pinCiclos[0], HIGH);
+      digitalWrite(pinCiclos[1], HIGH);
+      digitalWrite(pinCiclos[2], LOW);
+      digitalWrite(pinCiclos[3], LOW);
+      break;
+    case 3:
+      digitalWrite(pinCiclos[0], HIGH);
+      digitalWrite(pinCiclos[1], HIGH);
+      digitalWrite(pinCiclos[2], HIGH);
+      digitalWrite(pinCiclos[3], LOW);
+      break;
+    case 4:
+      digitalWrite(pinCiclos[0], HIGH);
+      digitalWrite(pinCiclos[1], HIGH);
+      digitalWrite(pinCiclos[2], HIGH);
+      digitalWrite(pinCiclos[3], HIGH);
+      break;
   }
+}
+
+bool botaoIniciarPressionado = false;  // flag para verificar o botão de iniciar
+bool botaoInterromperPressionado = false;
+long tempoInterrompido = 0;
+
+//Função para atualizar a máquina de estados
+void atualizaEstado(long intervaloSessao, long intervaloPausa) {
+  // Verifica o tempo decorrido em todos os estados
+  if (pausaIniciada) {
+    tempoPassado = millis() - inicioPausa;
+    Serial.print("Pausa decorrida: ");
+    Serial.print(tempoPassado);
+    Serial.println(" ms");
+    Serial.println(intervaloPausa);
+    if (tempoPassado < intervaloPausa) {
+      // Período de pausa ainda não terminou
+      return;
+    }
+  }
+  switch (estadoAtual) {
+    case INICIAL:
+      Serial.println("INICIAL");
+      cicloAtual = 0;
+      // AÇÕES PARA ESTADO INICIAL
+      atualizarLEDs(true, false);  // led verde ligado
+      atualizarLEDsCiclos(cicloAtual);
+
+      botaoIniciarPressionado = digitalRead(botaoIniciar) == HIGH;  // Atualiza a variável para indicar se o botão está pressionado
+
+      if (botaoIniciarPressionado) {
+        cicloAtual++;
+        estadoAtual = FOCO;
+        estadoAnterior = INICIAL;
+        sessaoIniciada = true;
+        if (!pausaIniciada) {
+          inicioFoco = millis();  // Regista o início do período de foco
+        }
+      }
+      break;
+
+    case FOCO:
+      // AÇÕES PARA ESTADO FOCO
+      atualizarLEDs(false, true);
+
+      if(cicloAtual > 4){
+        for (int i = 0; i < 4; i++) {
+        digitalWrite(pinCiclos[i], HIGH);
+        delay(100);
+      }
+      for (int i = 4; i > 0; i--) {
+        digitalWrite(pinCiclos[i], LOW);
+        delay(100);
+      }
+        estadoAnterior = FOCO;
+        estadoAtual = INICIAL;
+      }
+      atualizarLEDsCiclos(cicloAtual);
+
+      botaoInterromperPressionado = digitalRead(botaoInterromper) == HIGH;
+      if (botaoInterromperPressionado) {
+        estadoAtual = INTERROMPIDO;
+        estadoAnterior = FOCO;
+        tempoAnterior = millis() - inicioFoco;
+      }
+
+      if (sessaoIniciada) {
+        if (estadoAnterior == INTERROMPIDO) {
+          intervaloSessao -= tempoAnterior;
+        }
+        tempoPassado = millis() - inicioFoco;
+        Serial.print("Tempo decorrido: ");
+        Serial.print(tempoPassado);
+        Serial.println(" ms");
+        Serial.println(intervaloSessao);
+        if (tempoPassado < intervaloSessao) {
+          // Período de foco ainda não terminou
+          break;
+        } else {
+          estadoAtual = PAUSA;
+          estadoAnterior = FOCO;
+          pausaIniciada = true;
+          inicioPausa = millis();  // Regista o início do período de pausa
+          atualizarLEDs(true, false);
+        }
+      }
+      break;
+
+    case PAUSA:
+      // AÇÕES PARA ESTADO DE PAUSA
+      if (pausaIniciada) {
+        tempoPassado = millis() - inicioPausa;
+        Serial.print("Pausa decorrida: ");
+        Serial.print(tempoPassado);
+        Serial.println(" ms");
+        Serial.println(intervaloPausa);
+        if (tempoPassado < intervaloPausa) {
+          // Período de pausa ainda não terminou
+          break;
+        } else {
+          cicloAtual++;
+          estadoAtual = FOCO;
+          estadoAnterior = PAUSA;
+          inicioFoco = millis();  // Regista o início do próximo período de foco
+          atualizarLEDs(false, true);
+        }
+      }
+      break;
+
+    case INTERROMPIDO:
+      Serial.println("INTERROMPIDO");
+      digitalWrite(pinInterrompido, HIGH);
+
+      if (digitalRead(botaoIniciar) == HIGH) {
+        digitalWrite(pinInterrompido, LOW);
+        estadoAtual = estadoAnterior;
+
+        if (estadoAtual == FOCO) {
+          inicioFoco = millis() - tempoAnterior;  // Atualiza o tempo de início do período de foco com o valor anterior
+        } else if (estadoAtual == PAUSA) {
+          inicioPausa = millis() - tempoAnterior;  // Atualiza o tempo de início do período de pausa com o valor anterior
+        }
+        pausaIniciada = (estadoAtual == PAUSA);  // Atualiza a flag de pausa iniciada
+      }
+      break;
+  }
+}
+
+void loop() {
+  // Mapeia o valor do potenciómetro para um intervalo entre 0 e 3 (índices do array listaIntervalosSessao)
+  valorPotencia = analogRead(pinPotenciometro);
+  indiceIntervaloSessao = map(valorPotencia, 0, 687, 0, 3);
+  //intervaloSessao = listaIntervalosSessao[indiceIntervaloSessao] * 60000;  // passa o intervalo para milisegundos
+  intervaloSessao = 5000;
+  //intervaloPausa = 1 * 60000;  // 1 min em milisegundos
+  intervaloPausa = 5000;
+  //Serial.println(intervaloSessao);
+  atualizaEstado(intervaloSessao, intervaloPausa);
 }
